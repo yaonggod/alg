7f44584625f480c88a5ded6ecd4542d0572c69f2
Jooyong Lee <108652989+yaonggod@users.noreply.github.com>
2023-02-15 00:54:11 +0900

    [Gold V] Title: 노드사이의 거리, Time: 804 ms, Memory: 32452 KB -BaekjoonHub

---
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+for _ in range(n - 1):
+    a, b, length = map(int, input().split())
+    graph[a].append([b, length])
+    graph[b].append([a, length])
+for _ in range(m):
+    a, b = map(int, input().split())
+    visited = [False] * (n + 1)
+    visited[a] = True
+    queue = deque([[a, 0]])
+    while queue:
+        k = queue.popleft()
+        x = k[0]
+        length = k[1]
+        if x == b:
+            print(length)
+        for y in graph[x]:
+            if not visited[y[0]]:
+                visited[y[0]] = True
+                queue.append([y[0], length + y[1]])
