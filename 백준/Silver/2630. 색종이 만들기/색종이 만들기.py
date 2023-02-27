4b8830dd635147b2188319d27ab828be3500b82a
Jooyong Lee <108652989+yaonggod@users.noreply.github.com>
2023-02-28 00:09:32 +0900

    Update 색종이 만들기.py

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -9,11 +9,13 @@ for _ in range(n):
 white_count = 0
 blue_count = 0
 
+# (0, 0)에서 n의 길이로 시작해서
 def pap(length, sr, sc):
     global white_count
     global blue_count
     blue = False
     white = False
+    # length * length 영역 탐색해서 한 가지 색으로 통일되었는지 검사
     for i in range(sr, sr + length):
         for j in range(sc, sc + length):
             if paper[i][j] == 0:
@@ -22,12 +24,15 @@ def pap(length, sr, sc):
                 blue = True
             if blue and white:
                 break
+    # 두 색깔이 모두 나오면 길이를 반으로 나눠서 4개 영역으로 나눈 후 탐색
     if blue and white:
+        # 길이가 0으로 가지 않게
         if length // 2 > 0:
             pap(length // 2, sr, sc)
             pap(length // 2, sr + length // 2, sc)
             pap(length // 2, sr, sc + length // 2)
             pap(length // 2, sr + length // 2, sc + length // 2)
+    # 한 색깔만 나오면 카운트 += 1
     elif blue and not white:
         blue_count += 1
     elif not blue and white:
