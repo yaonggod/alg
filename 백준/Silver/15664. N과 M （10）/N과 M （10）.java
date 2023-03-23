d5b26e6e0667a21ece3d2721c1f3c563aa8a23aa
Jooyong Lee <108652989+yaonggod@users.noreply.github.com>
2023-03-23 10:56:14 +0900

    [Silver II] Title: N과 M (10), Time: 232 ms, Memory: 18376 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+	static int n = 0;
+	static int m = 0;
+	static Map<Integer, Integer> map = new HashMap<>();
+	static int[] arr = null;
+	static int[] newarr = null;
+	static Integer[] keyset = null;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		arr = new int[n];
+		newarr = new int[m];
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		for (int i = 0; i < n; i++) {
+			if (map.containsKey(arr[i])) {
+				map.put(arr[i], map.get(arr[i]) + 1);
+			} else {
+				map.put(arr[i], 1);
+			}
+		}
+		keyset = map.keySet().toArray(new Integer[0]);
+		Arrays.sort(keyset);
+		backtracking(0, 0);
+		System.out.println(sb);
+	}
+	
+	public static void backtracking(int key, int c) {
+		if (c == m) {
+			for (int i = 0; i < m; i++) {
+				sb.append(newarr[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = key; i < keyset.length; i++) {
+			if (map.get(keyset[i]) > 0) {
+				map.put(keyset[i], map.get(keyset[i]) - 1);
+				newarr[c] = keyset[i];
+				backtracking(i, c + 1);
+				map.put(keyset[i], map.get(keyset[i]) + 1);
+			}
+		}
+	}
+}
