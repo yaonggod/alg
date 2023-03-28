041528077e0e228672520da855ed61451c6e99f8
Jooyong Lee <108652989+yaonggod@users.noreply.github.com>
2023-03-28 10:23:16 +0900

    [Gold IV] Title: 최단경로, Time: 2068 ms, Memory: 323764 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -5,7 +5,6 @@ import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Main {
-	// 어떤 node에서 end로 가는 거리
 	public static class Edge implements Comparable<Edge> {
 		int end;
 		int cost;
@@ -15,8 +14,7 @@ public class Main {
 			this.end = end;
 			this.cost = cost;
 		}
-		
-		//순오름차순
+
 		@Override
 		public int compareTo(Edge o) {
 			return this.cost - o.cost;
@@ -49,17 +47,14 @@ public class Main {
 		Arrays.fill(route, Integer.MAX_VALUE);
 		route[k] = 0;
 		PriorityQueue<Edge> pq = new PriorityQueue<>();
-		// pq에 k -> end로 가는 cost 정보 저장
+		boolean[] visited = new boolean[v + 1];
 		pq.offer(new Edge(k, 0));
 		while (!pq.isEmpty()) {
 			Edge x = pq.poll();
-			// x에서 y로 갈 수 있는데
 			for (Edge y : graph[x.end]) {
-				// k에서 y로 갈 때 x를 통과하는게 더 빠름
 				// k -> y > k -> x + x -> y
 				if (route[y.end] > route[x.end] + y.cost) {
 					route[y.end] = route[x.end] + y.cost;
-					// k -> y 최소값 갱신해서 pq에 넣어줌
 					pq.offer(new Edge(y.end, route[y.end]));
 				}				
 			}	
