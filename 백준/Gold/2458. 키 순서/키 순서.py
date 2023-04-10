cd1820824bf097cba512f9debbf22cc56952c06c
Jooyong Lee <108652989+yaonggod@users.noreply.github.com>
2023-04-10 12:35:46 +0900

    [Gold IV] Title: 키 순서, Time: 1316 ms, Memory: 117216 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.py
+++ 백준/Gold/2458. 키 순서/키 순서.py
@@ -6,14 +6,12 @@ count = [[0] * (n + 1) for _ in range(n + 1)]
 
 for i in range(m):
     a, b = map(int, input().split())
-    # a보다 b가 크다
     count[a][b] = 1
 
 for k in range(1, n + 1):
     for i in range(1, n + 1):
         for j in range(1, n + 1):
             if i != j:
-                # i보다 k가 크고 k보다 j가 크면 i보다 j가 큰게 맞음
                 if count[i][k] == 1 and count[k][j] == 1:
                     count[i][j] = 1
 
@@ -21,11 +19,8 @@ c = 0
 for i in range(1, n + 1):
     s = 0
     for j in range(1, n + 1):
-        # i보다 큰 사람
         s += count[i][j]
-        # i보다 작은 사람
         s += count[j][i]
-    # 정확히 자기 자신을 제외한 사람 수와 같음
     if s == n - 1:
         c += 1
     
