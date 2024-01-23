e3a99252f7d7d342c42b367cabe9f86321ce9479
Jooyong Lee <jy23698@naver.com>
2024-01-24 07:13:57 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        int index1 = 0;
+        int index2 = 0;
+        boolean success = true;
+        for (int i = 0; i < goal.length; i++) {
+            if (index1 < cards1.length && cards1[index1].equals(goal[i])) index1++;
+            else if (index2 < cards2.length && cards2[index2].equals(goal[i])) index2++;
+            else {
+                success = false;
+                break;
+            }
+        }
+        return success ? "Yes" : "No";
+    }
+}
