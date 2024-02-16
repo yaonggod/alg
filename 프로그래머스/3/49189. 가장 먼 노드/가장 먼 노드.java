d50d9aef5246a44460813fa8cd01a4cf9d100bce
Jooyong Lee <jy23698@naver.com>
2024-02-16 19:51:49 +0900

    [level 3] Title: 가장 먼 노드, Time: 31.17 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,35 +1,34 @@
 import java.util.*;
 class Solution {
     public int solution(int n, int[][] edge) {
-        boolean[][] graph = new boolean[n + 1][n + 1];
+        List<Integer>[] graph = new List[n + 1];
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
         for (int i = 0; i < edge.length; i++) {
-            graph[edge[i][0]][edge[i][1]] = true;
-            graph[edge[i][1]][edge[i][0]] = true;
+            graph[edge[i][0]].add(edge[i][1]);
+            graph[edge[i][1]].add(edge[i][0]);
         }
         int[] visited = new int[n + 1];
         visited[1] = 1;
-        int maxLen = 0;
-        int count = 0;
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(1);
         while (!queue.isEmpty()) {
-            int s = queue.poll();
-            for (int i = 1; i <= n; i++) {
-                if (graph[s][i] == true && visited[i] == 0) {
-                    visited[i] = visited[s] + 1;
-                    queue.offer(i);
+            int from = queue.poll();
+            for (int to : graph[from]) {
+                if (visited[to] == 0) {
+                    visited[to] = visited[from] + 1;
+                    queue.offer(to);
                 }
             }
         }
-        for (int i = 0; i <= n; i++) {
-            if (visited[i] > maxLen) {
-                maxLen = visited[i];
-            }
+        int maxLen = 0;
+        int count = 0;
+        for (int i = 2; i <= n; i++) {
+            if (visited[i] > maxLen) maxLen = visited[i];
         }
-        for (int i = 0; i <= n; i++) {
-            if (visited[i] == maxLen) {
-                count++;
-            }
+        for (int i = 2; i <= n; i++) {
+            if (visited[i] == maxLen) count++;
         }
         return count;
     }
