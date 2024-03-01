cc5e2a3d151200cd70e35799aad014062e876564
Jooyong Lee <jy23698@naver.com>
2024-03-01 22:06:27 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 51.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int jump = 0;
+        while (n != 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n -= 1;
+                jump++;
+            }
+        }
+        return jump;
+    }
+}
