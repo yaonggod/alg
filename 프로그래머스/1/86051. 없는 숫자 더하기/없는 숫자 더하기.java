4f552aded35ccddc524d6b4ff093b43665cbcb15
Jooyong Lee <jy23698@naver.com>
2024-03-08 16:13:09 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.84 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,9 +1,7 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] numbers) {
-        int sum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            sum += numbers[i];
-        }
-        return 45 - sum;
+        return 45 - Arrays.stream(numbers).sum();
     }
 }
