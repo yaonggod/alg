e8fb7ba7ab0c68c98bf3ceb065f6f3085d623dce
Jooyong Lee <jy23698@naver.com>
2024-05-03 03:19:18 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.23 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,13 +1,12 @@
 import java.util.*;
 
 class Solution {
-    static int gap = 100;
+    static int gap, N;
     static List<Integer>[] map;
-    static int[][] w;
-    static int tower; 
+    static boolean[] visited;
     public int solution(int n, int[][] wires) {
-        w = wires;
-        tower = n;
+        gap = 100;
+        N = n;
         map = new ArrayList[n + 1];
         for (int i = 1; i <= n; i++) {
             map[i] = new ArrayList<>();
@@ -16,41 +15,25 @@ class Solution {
             map[wires[i][0]].add(wires[i][1]);
             map[wires[i][1]].add(wires[i][0]);
         }
-        
-        for (int i = 0; i < wires.length; i++) {
-            disconnect(i);
-        }
+        visited = new boolean[n + 1];
+        visited[1] = true;
+        dfs(1);
         
         return gap;
     }
     
-    // index번째 전선 끊기
-    static void disconnect(int index) {
-        int a = w[index][0];
-        int b = w[index][1];
-        
-        boolean[] visited = new boolean[tower + 1];
-        visited[1] = true;
-        
-        // 1번을 중심으로 연결된 모든 곳 탐색
-        int size = 1;
-        Queue<Integer> connect = new LinkedList<>();
-        connect.offer(1);
-        while (!connect.isEmpty()) {
-            int from = connect.poll();
-            for (int to : map[from]) {
-                // 끊어진 곳이 아니면 가라
-                if (!(from == a && to == b) && !(from == b && to == a) && !visited[to]) {
-                    visited[to] = true;
-                    connect.offer(to);
-                    size++;
-                }
+    static int dfs(int current) {
+        // 나의 자식 노드의 개수
+        int total = 0;
+        for (int to : map[current]) {
+            if (!visited[to]) {
+                visited[to] = true;
+                int count = dfs(to);
+                gap = Math.min(Math.abs(N - count - count), gap);
+                total += count;
             }
         }
-        
-        // 크기 차이
-        if (Math.abs(size * 2 - tower) < gap) {
-            gap = Math.abs(size * 2 - tower);
-        }
+        // 내 자식 노드 + 나
+        return total + 1;
     }
 }
