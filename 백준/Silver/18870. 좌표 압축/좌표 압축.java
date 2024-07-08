3933e9e8f35084957b40721c019d0eb416741248
Jooyong Lee <jy23698@naver.com>
2024-07-09 01:48:19 +0900

    [Silver II] Title: 좌표 압축, Time: 1864 ms, Memory: 267212 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        TreeSet<Integer> ts = new TreeSet<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            arr[i] = num;
+            ts.add(num);
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int idx = 0;
+        for (Integer num : ts) {
+            map.put(num, idx++);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(map.get(arr[i])).append(" ");
+        }
+        System.out.println(sb);
+    }
+
+}
