3be317145fc7db9a8af00864136a3cdcac1a2047
Jooyong Lee <jy23698@naver.com>
2024-08-06 22:56:12 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13892 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+
+    }
+
+}
