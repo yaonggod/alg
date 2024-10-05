cc4e2ec5e989b69746435c8b311c57c5a1665a3d
Jooyong Lee <jy23698@naver.com>
2024-10-05 13:42:51 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10724 ms, Memory: 305184 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -1,63 +1,67 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// 컴퓨터
-		String[] line = br.readLine().split(" ");
-		int n = Integer.parseInt(line[0]);
-		int m = Integer.parseInt(line[1]);
-		List<Integer>[] computer = new ArrayList[n + 1];
-		for (int i = 1; i <= n; i++) {
-			computer[i] = new ArrayList<>();
-		}
-		// B를 해킹하면 A를 해킹할 수 있다
-		// 역방향은 성립하지 않음
-		for (int i = 0; i < m; i++) {
-			line = br.readLine().split(" ");
-			int A = Integer.parseInt(line[0]);
-			int B = Integer.parseInt(line[1]);
-			computer[B].add(A);
-		}
-		int[] count = new int[n + 1];
-		for (int i = 1; i <= n; i++) {
-			Queue<Integer> queue = new LinkedList<>();
-			queue.offer(i);
-			boolean[] visited = new boolean[n + 1];
-			visited[i] = true;
-			count[i] = 1;
-			while (!queue.isEmpty()) {
-				int x = queue.poll();
-				for (int y : computer[x]) {
-					if (!visited[y]) {
-						// 감염시켰음
-						// 방문처리
-						visited[y] = true;
-						// 감염된 컴퓨터++
-						count[i]++;
-						// 큐에 넣기
-						queue.offer(y);
-					}
-				}
-			}
-		}
-		int maxCnt = 0;
-		for (int i = 1; i <= n; i++) {
-			if (count[i] > maxCnt) {
-				maxCnt = count[i];
-			}
-		}
-		for (int i = 1; i <= n; i++) {
-			if (count[i] == maxCnt) {
-				System.out.print(i + " ");
-			}
-		}
-	}	
+    static int[] treeSize;
+    static boolean[] visited;
+    static ArrayList<Integer>[] tree;
+    static int n, m;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        treeSize = new int[n + 1];
+
+        tree = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            tree[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            // 애기
+            int b = Integer.parseInt(st.nextToken());
+            // 부모
+            int p = Integer.parseInt(st.nextToken());
+            // 애기 -> 부모 타고들어가서 부모의 애기 하나씩 더해줌
+            tree[b].add(p);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            visited = new boolean[n + 1];
+            bfs(i);
+        }
+
+
+        int maxCom = 0;
+        for (int i = 1; i <= n; i++) {
+            if (treeSize[i] > maxCom) {
+                maxCom = treeSize[i];
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            if (treeSize[i] == maxCom) {
+               sb.append(i).append(" ");
+            }
+        }
+        System.out.println(sb);
+    }
+    static void bfs(int p) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(p);
+        visited[p] = true;
+        while (!queue.isEmpty()) {
+            int from = queue.poll();
+            for (int to : tree[from]) {
+                if (!visited[to]) {
+                    visited[to] = true;
+                    treeSize[to]++;
+                    queue.add(to);
+                }
+            }
+        }
+    }
 }
