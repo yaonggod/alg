bb00eab6cc9b3ae2fe53942db19586bdd2b79166
Jooyong Lee <jy23698@naver.com>
2024-10-06 02:39:41 +0900

    [Silver I] Title: 단지번호붙이기, Time: 108 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class Main {
     static int n;
     static char[][] house;
-    static boolean[][] visited;
     static int[] dx = new int[] {0, 1, 0, -1};
     static int[] dy = new int[] {1, 0, -1, 0};
     static boolean valid(int x, int y) {
@@ -16,16 +15,15 @@ public class Main {
         n = Integer.parseInt(br.readLine());
 
         house = new char[n][n];
-        visited = new boolean[n][n];
         for (int i = 0; i < n; i++) {
             house[i] = br.readLine().toCharArray();
         }
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (house[i][j] == '1' && !visited[i][j]) {
+                if (house[i][j] == '1') {
                     int count = 1;
-                    visited[i][j] = true;
+                    house[i][j] = '0';
                     Queue<Integer[]> queue = new LinkedList<>();
                     queue.offer(new Integer[] {i, j});
                     while (!queue.isEmpty()) {
@@ -33,8 +31,8 @@ public class Main {
                         for (int d = 0; d < 4; d++) {
                             int nx = from[0] + dx[d];
                             int ny = from[1] + dy[d];
-                            if (valid(nx, ny) && house[nx][ny] == '1' && !visited[nx][ny]) {
-                                visited[nx][ny] = true;
+                            if (valid(nx, ny) && house[nx][ny] == '1') {
+                                house[nx][ny] = '0';
                                 count++;
                                 queue.offer(new Integer[] {nx, ny});
                             }
