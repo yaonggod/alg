bde620efea05a1aa7f1d31aac794d010b2db4f14
Jooyong Lee <jy23698@naver.com>
2024-10-13 14:43:18 +0900

    [level 1] Title: 옹알이 (2), Time: 1.71 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/README.md
+++ 프로그래머스/1/133499. 옹알이 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 1] 옹알이 (2) - 133499 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133499#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133499) 
 
 ### 성능 요약
 
-메모리: 75.7 MB, 시간: 1.67 ms
+메모리: 80 MB, 시간: 1.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 13일 11:43:20
+2024년 10월 13일 14:43:13
 
 ### 문제 설명
 
--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,49 +1,28 @@
 import java.util.*;
 
 class Solution {
+    static String[] no = new String[] {"ayaaya", "yeye", "woowoo", "mama"};
+    static String[] yes = new String[] {"aya", "ye", "woo", "ma"};
     public int solution(String[] babbling) {
         int answer = 0;
         
         for (String b : babbling) {
-            StringBuilder sb = new StringBuilder();
-            char[] bChar = b.toCharArray();
-            int n = bChar.length;
-            int before = 0;
-            
-            for (char c : bChar) {
-                sb.append(c);
-                int l = sb.length();
-                if (l >= 2) {
-                    if (sb.substring(l - 2, l).equals("ye") && before != 1) {
-                        sb.setCharAt(l - 2, '0');
-                        sb.setCharAt(l - 1, '0');
-                        before = 1;
-                    } else if (sb.substring(l - 2, l).equals("ma") && before != 2) {
-                        sb.setCharAt(l - 2, '0');
-                        sb.setCharAt(l - 1, '0');
-                        before = 2;
-                    }
-                } 
-                if (l >= 3) {
-                    if (sb.substring(l - 3, l).equals("aya") && before != 3) {
-                        sb.setCharAt(l - 3, '0');
-                        sb.setCharAt(l - 2, '0');
-                        sb.setCharAt(l - 1, '0');
-                        before = 3;
-                    } else if (sb.substring(l - 3, l).equals("woo") && before != 4) {
-                        sb.setCharAt(l - 3, '0');
-                        sb.setCharAt(l - 2, '0');
-                        sb.setCharAt(l - 1, '0');
-                        before = 4;
-                    }
-                }
-            }
-            StringBuilder compare = new StringBuilder();
-            for (int i = 0; i < n; i++) {
-                compare.append('0');
-            }
-            if (sb.toString().equals(compare.toString())) answer++;
+            answer += possible(b);
         }
         return answer;
+    }
+    
+    static int possible(String bab) {
+        for (String n : no) {
+            if (bab.contains(n)) return 0;
+        }
+        
+        for (String y : yes) {
+            bab = bab.replace(y, " ");
+        }
+        bab = bab.replace(" ", "");
+        
+        if (bab.length() == 0) return 1;
+        return 0;
     }
 }
