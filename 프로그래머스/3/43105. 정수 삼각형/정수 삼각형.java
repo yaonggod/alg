857d505505d7f18fc49f935fe3cc1503b135544c
Jooyong Lee <jy23698@naver.com>
2024-12-26 22:12:19 +0900

    [level 3] Title: 정수 삼각형, Time: 9.81 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,13 +1,18 @@
 class Solution {
     public int solution(int[][] triangle) {
+        // 제일 마지막 행의 길이 1 2 3 4 ... n
         int n = triangle.length;
         
+        // 두 번째 행부터 위의 행 값 참고해서 값 갱신
         for (int i = 1; i < n; i++) {
             for (int j = 0; j <= i; j++) {
+                // 제일 왼쪽거
                 if (j == 0) {
                     triangle[i][j] += triangle[i - 1][j];
+                // 제일 오른쪽거
                 } else if (j == i) {
                     triangle[i][j] += triangle[i - 1][j - 1];
+                // 내 바로 위에거, 그 왼쪽거랑 비교
                 } else {
                     if (triangle[i - 1][j - 1] > triangle[i - 1][j]) {
                         triangle[i][j] += triangle[i - 1][j - 1];
